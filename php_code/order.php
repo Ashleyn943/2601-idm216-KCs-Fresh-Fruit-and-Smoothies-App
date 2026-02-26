f5efde9c7cd5eed4d4be41c9194fb19c7134b6c3
Ashley <an943@drexel.edu>
2026-02-25 19:14:50 -0500

    update name on order.php

--- php_code/order.php
+++ php_code/order.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="normalize" href="css/normalize.css">
     <link rel="stylesheet" href="css/stylesheet.css">
-    <title>Main Menu</title>
+    <title>Order</title>
 </head>
 <body>
     <h1>KC's Menu</h1>
