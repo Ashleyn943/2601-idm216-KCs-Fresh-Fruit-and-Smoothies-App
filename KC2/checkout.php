547ae3543f874a23374aeb47b64b58d3692bdc40
Ashley <an943@drexel.edu>
2026-03-17 21:48:00 -0400

    fix to checkout back button + image problem

--- KC2/checkout.php
+++ KC2/checkout.php
@@ -36,7 +36,7 @@
 
         <!-- Header -->
         <div class="page-header">
-            <a href="index.php" class="back-button">
+            <a href="bag.php" class="back-button">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
